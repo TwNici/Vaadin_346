3dd6da8d0596141e90c4ee754d8d1082af2d3574
CHNI4 <nicolas.chapatte@coop.ch>
2024-12-20 11:20:35 +0100

    Added Buttons

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -15,7 +15,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
-@Theme("my-theme")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
